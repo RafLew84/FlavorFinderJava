2beeea6cb43e8d8c7a3d68644b456030b3b31cc4
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-09-29 15:10:40 +0200

    gradle setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id ("androidx.navigation.safeargs")
 }
 
 android {
@@ -25,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -32,6 +36,21 @@ android {
 }
 
 dependencies {
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.google.code.gson:gson:2.9.1")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // ROOM
+    implementation ("androidx.room:room-runtime:2.5.2")
+    annotationProcessor ("androidx.room:room-compiler:2.5.2")
+
+    // ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.5.1")
+
+    implementation("io.coil-kt:coil:2.4.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
